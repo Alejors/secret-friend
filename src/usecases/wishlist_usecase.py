a540450a26e6d5f2d6577edcfaba90f1a52bfe49
Alejandro Atria <alejandro.atria@enviame.io>
2024-11-11 18:12:20 -0500

    Create Wishes updated

--- src/usecases/wishlist_usecase.py
+++ src/usecases/wishlist_usecase.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.models import Wish
 from src.interfaces import IDataRepository
 
@@ -7,18 +9,35 @@ class ManageWishlistUsecase:
     self._wishlist_repository = wishlist_repository
   
   def get_wishlist_by_user_and_event(self, user_id: int, event_id: int) -> list[Wish]:
-    filter = {"user_id": user_id, "event_id": event_id}
+    filter = {"user_id": user_id, "event_id": event_id, "deleted_at": None}
     return self._wishlist_repository.get(filters=filter)
+  
+  def get_wish_by_user_element_and_event(self, user_id: int, event_id: int, element: str) -> Wish:
+    filter = {"user_id": user_id, "element": element, "event_id": event_id}
+    return self._wishlist_repository.get(filters=filter, first_only=True)
   
   def create_wishes(self, user_id: int, data: dict) -> tuple[list[Wish]|None, str|None]:
     event_id = data["event_id"]
     wishes = data["wishes"]
     try:
+      current_wishlist = self.get_wishlist_by_user_and_event(user_id, event_id)
+      wish_names = [wish.element for wish in current_wishlist if wish]
       for element in wishes:
         element["user_id"] = user_id
         element["event_id"] = event_id
         wish = Wish.from_dict(element)
-        self._wishlist_repository.insert(wish)
+        if wish.element not in wish_names:
+          removed_wish = self.get_wish_by_user_element_and_event(user_id, event_id, wish.element)
+          if removed_wish and removed_wish.deleted_at is not None:
+            renewal = {"deleted_at": None}
+            self._wishlist_repository.update(removed_wish.id, renewal)
+          else:
+            self._wishlist_repository.insert(wish)
+      new_wishes_names = list(map(lambda wish: wish["element"], wishes))
+      wishes_to_remove = list(filter(lambda wish: wish.element not in new_wishes_names, current_wishlist))
+      for wish_to_remove in wishes_to_remove:
+        removal = {"deleted_at": datetime.now()}
+        self._wishlist_repository.update(wish_to_remove.id, removal)
       
       return self.get_wishlist_by_user_and_event(user_id, event_id), None
     except Exception as e:
