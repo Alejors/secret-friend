c6324c16e176ceb23f514790d1a1673c1a165078
Alejandro Atria <alejandro.atria@enviame.io>
2024-11-15 17:07:51 -0500

    flask configs to allow csrf token in form

--- src/frameworks/http/flask.py
+++ src/frameworks/http/flask.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 from flask_jwt_extended import JWTManager
 
 
@@ -8,19 +7,17 @@ def create_flask_app(blueprints, commands: dict):
   template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "templates"))
   static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "static"))
   app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
-  
-  
+
   app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY")
-  app.config['JWT_TOKEN_LOCATION'] = ['cookies', 'headers']
-  app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+  app.config['JWT_TOKEN_LOCATION'] = ['cookies', 'headers', 'query_string']
   app.config['DEBUG'] = True # DEV
   app.config['TEMPLATES_AUTO_RELOAD'] = True # DEV
-  app.secret_key = os.environ.get("FLASK_SECRET_KEY")
+  app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+  app.config['WTF_CSRF_ENABLED'] = True
+  app.config["JWT_COOKIE_SECURE"] = False
+  app.secret_key = os.environ.get("JWT_SECRET_KEY")
 
   jwt = JWTManager(app)
-  # csrf = CSRFProtect(app)
-  
-  app.json.sort_keys = False
 
   for blueprint in blueprints:
     app.register_blueprint(blueprint)
