a33f093bb31a5d7394cbb30cb49123cab9cf34c9
Alejandro Atria <alejandro.atria@enviame.io>
2024-11-15 17:33:47 -0500

    modify error message to email validator

--- src/templates/forms/registry.py
+++ src/templates/forms/registry.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Email, Regexp
 
 class RegistryForm(FlaskForm):
   name = StringField('Nombre', validators=[DataRequired()])
-  email = StringField('Email', validators=[DataRequired(), Email()])
-  password = PasswordField('Contraseña', validators=[DataRequired(), Regexp(r"^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,12}$", message="La contraseña debe tener entr 8 y 12 caracteres, al menos 1 mayúscula y al menos 1 número!")])
+  email = StringField('Email', validators=[DataRequired(), Email(message="Email no es válido.")])
+  password = PasswordField('Contraseña', validators=[DataRequired(), Regexp(r"^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,12}$", message="La contraseña debe tener entre 8 y 12 caracteres, al menos 1 mayúscula y al menos 1 número!")])
   
   submit = SubmitField('Registrarse')
