974aeaecee7675ad3909cc8868777b2998cd6b5d
Nico Araya <nicolasaraya@gmail.com>
2024-11-29 21:07:58 -0300

    Fix - Wishlist - Image URL on image upload

--- src/frameworks/bucket/client.py
+++ src/frameworks/bucket/client.py
@@ -5,34 +5,44 @@ import boto3
 class BucketClient:
   def __init__(self):
     self.url = os.environ.get("BUCKET_URL")
+    self._region = os.environ.get("AWS_REGION")
     self._client = boto3.client(
       's3',
-      endpoint_url=self.url,
       aws_access_key_id=os.environ.get("BUCKET_ACCESS_KEY"),
-      aws_secret_access_key=os.environ.get("BUCKET_SECRET_KEY")    
+      aws_secret_access_key=os.environ.get("BUCKET_SECRET_KEY"),
+      region_name=self._region
     )
     self._bucket = os.environ.get("BUCKET_NAME")
-    self._check_bucket(self._bucket)
+    self._check_bucket()
   
-  def _check_bucket(self, bucket_name: str):
+  def _check_bucket(self):
     if self._bucket not in self._get_buckets():
       self._client.create_bucket(Bucket=self._bucket)
       print("BUCKET CREATED...")
     else:
       print("BUCKET ALREADY EXISTS...")
-  
+
   def _get_buckets(self):
-    return self._client.list_buckets()
-    
-  def upload_file(self, file, user_id, bucket_name:str = None):
-    object_name = f"{user_id}_{file.filename}"
-    if bucket_name:
-      bucket = f"{self._bucket}/{bucket_name}"
+    response = self._client.list_buckets().get("Buckets")
+    if response:
+      buckets = [bucket["Name"] for bucket in self._client.list_buckets().get("Buckets")]
     else:
-      bucket = self._bucket
+      buckets = []
+    return buckets
+    
+  def upload_file(self, file, user_id, folder:str = None):
+    object_name = f"{folder}/{user_id}_{file.filename}"
     try:
-      self._client.upload_fileobj(file.stream, bucket, object_name, ExtraArgs={"ACL": "public-read"})
-      return f"{self.url.replace("ninja", "localhost")}/{bucket}/{object_name}?noAuth=true" # AJUSTAR ANTES DE DESPLEGAR
+      self._client.upload_fileobj(
+        file.stream,
+        self._bucket,
+        object_name,
+        ExtraArgs={"ContentType": "image/jpeg"})
+
+      if os.environ.get("ENVIRONMENT") == "local":
+        return f"{self.url.replace("ninja", "localhost")}{object_name}?noAuth=true"
+      else:
+        return f"{self.url}{object_name}"
     except Exception as e:
       print(f"An error occurred: {e}")
       return None
