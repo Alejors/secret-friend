f490afceefc8d542c9d26a65a1e146494901e20c
Nico Araya <nicolasaraya@gmail.com>
2024-12-01 19:56:09 -0300

    Add - Logs folder if needed

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo “Starting server...”
 cd /home/ec2-user/projects/secret-friend
+mkdir -p /logs
+
 /usr/local/bin/docker-compose -f docker-compose.prod.yml build
 /usr/local/bin/docker-compose -f docker-compose.prod.yml up -d
 /usr/local/bin/docker-compose -f docker-compose.prod.yml exec api alembic upgrade head  > /logs/alembic_upgrade.log 2>&1
