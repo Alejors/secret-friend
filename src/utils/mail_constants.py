cf99a791946caa81a6921a962aa4e0851f1c8acf
Alejandro Atria <alejandro.atria@enviame.io>
2024-12-05 12:55:52 -0500

    commented website link elements

--- src/utils/mail_constants.py
+++ src/utils/mail_constants.py
@@ -7,10 +7,12 @@ HEADER = """
     </div>"""
 
 FOOTER = """
-    <p>Recuerda que puedes actualizar tu lista de deseos ingresando a <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">la aplicación</a>!</p>
+    <!--
+    <p>Recuerda que puedes actualizar tu lista de deseos ingresando a <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">la aplicación</a>!</p>-->
     <br/>
     <p>Saludos!</p>
+    <!--
     <br/>
-    <small>Te recordamos que si no tenías una cuenta creada antes de que se te incorporara en este evento, se creó una automáticamente. <br/>Tu usuario es tu correo electrónico y tu contraseña inicial es el nombre del evento en minúsculas, reemplazando los espacios por guión bajo "_".<br/>Por ejemplo: "Ejemplo 2024" sería "ejemplo_2024"</small>
+    <small>Te recordamos que si no tenías una cuenta creada antes de que se te incorporara en este evento, se creó una automáticamente. <br/>Tu usuario es tu correo electrónico y tu contraseña inicial es el nombre del evento en minúsculas, reemplazando los espacios por guión bajo "_".<br/>Por ejemplo: "Ejemplo 2024" sería "ejemplo_2024"</small> -->
   </body>
 </html>"""
