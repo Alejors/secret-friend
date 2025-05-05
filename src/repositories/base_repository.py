22620f2c8d65d2a0402d9a4cfb2c70dbd47bf228
Alejandro Atria <alejandro.atria@enviame.io>
2025-05-05 14:32:25 -0500

    modificación del repositorio base de sqlalchemy para retornar entidades de dominio.

--- src/repositories/base_repository.py
+++ src/repositories/base_repository.py
@@ -2,49 +2,64 @@ from src.interfaces import IDataRepository
 
 
 class SQLAlchemyBaseRepository(IDataRepository):
-    def __init__(self, sqlalchemy_client, query_class=None):
+    def __init__(self, sqlalchemy_client, query_class=None, entity=None):
+        """
+        Constructor del repositorio base.
+
+        :param sqlalchemy_client: Cliente de SQLAlchemy para manejar sesiones.
+        :param query_class: Modelo de base de datos asociado.
+        :param entity: Clase de la entidad de dominio asociada.
+        """
         self.session_factory = sqlalchemy_client.session_factory
         self.query_class = query_class
+        self.entity = entity
 
     def get(
-      self,
-      *,
-      filters: dict = None,
-      first_only: bool = False,
+        self,
+        *,
+        filters: dict = None,
+        first_only: bool = False,
     ):
-      with self.session_factory() as session:
-        query = session.query(self.query_class)
+        with self.session_factory() as session:
+            query = session.query(self.query_class)
 
-        if filters:
-          query = query.filter_by(**filters)
+            if filters:
+                query = query.filter_by(**filters)
 
-        if first_only:
-          results = query.first()
-          return results
-        else:
-          results = query.all()
-          return results
+            if first_only:
+                results = query.first()
+                if self.entity:
+                    return self.entity.from_model(results)
+                return results
+            else:
+                results = query.all()
+                if self.entity:
+                    return [self.entity.from_model(result) for result in results]
+                return results
 
     def insert(
         self,
-        instance,
+        dict: dict,
     ):
-      with self.session_factory() as session:
-        session.add(instance)
-        session.commit()
-
-        return instance
+        with self.session_factory() as session:
+            instance = self.query_class.from_dict(dict)
+            session.add(instance)
+            session.commit()
+            if self.entity:
+                return self.entity.from_model(instance)
+            return instance
 
     def update(
-      self,
-      id: int,
-      fields: dict,
+        self,
+        id: int,
+        fields: dict,
     ):
-      with self.session_factory() as session:
-        query = session.query(self.query_class).filter_by(id=id)
-        query.update(fields)
-        instance = query.first()
+        with self.session_factory() as session:
+            query = session.query(self.query_class).filter_by(id=id)
+            query.update(fields)
+            instance = query.first()
 
-        session.commit()
-
-        return instance
+            session.commit()
+            if self.entity:
+                return self.entity.from_model(instance)
+            return instance
