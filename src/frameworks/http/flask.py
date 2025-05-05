7c315a09ef50436c3705e1640c12b4b6f211c179
Alejandro Atria <alejandro.atria@enviame.io>
2025-05-05 16:06:22 -0500

    add global error handler

--- src/frameworks/http/flask.py
+++ src/frameworks/http/flask.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import Flask, redirect, url_for
+from flask_jwt_extended.exceptions import NoAuthorizationError
 from jinja2 import Environment
 from flask_jwt_extended import JWTManager
 
@@ -7,36 +8,44 @@ from src.frameworks.jinja.custom_filters import custom_methods
 
 
 def create_flask_app(blueprints, commands: dict):
-  template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "templates"))
-  static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "static"))
-  app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+    template_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "templates")
+    )
+    static_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "static")
+    )
+    app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
-  environment = os.environ.get("ENVIRONMENT")
-  app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY")
-  app.config['JWT_TOKEN_LOCATION'] = ['cookies', 'headers', 'query_string']
-  if environment == "local":
-    app.config['DEBUG'] = True # DEV
-    app.config['TEMPLATES_AUTO_RELOAD'] = True # DEV
-  else:
-    app.config['JWT_COOKIE_DOMAIN'] = os.environ.get("DOMAIN")
-    app.config['JWT_COOKIE_SAMESITE'] = 'Lax'
-  app.config['JWT_COOKIE_CSRF_PROTECT'] = False
-  app.config['WTF_CSRF_ENABLED'] = True
-  app.config["JWT_COOKIE_SECURE"] = False
-  app.secret_key = os.environ.get("JWT_SECRET_KEY")
+    environment = os.environ.get("ENVIRONMENT")
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+    app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers", "query_string"]
+    if environment == "local":
+        app.config["DEBUG"] = True  # DEV
+        app.config["TEMPLATES_AUTO_RELOAD"] = True  # DEV
+    else:
+        app.config["JWT_COOKIE_DOMAIN"] = os.environ.get("DOMAIN")
+        app.config["JWT_COOKIE_SAMESITE"] = "Lax"
+    app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+    app.config["WTF_CSRF_ENABLED"] = True
+    app.config["JWT_COOKIE_SECURE"] = environment == "production"
+    app.secret_key = os.environ.get("JWT_SECRET_KEY")
 
-  jinja_env = Environment()
-  for filter, method in custom_methods.items():
-    jinja_env.filters[filter] = method
+    jinja_env = Environment()
+    for filter, method in custom_methods.items():
+        jinja_env.filters[filter] = method
 
-  app.jinja_env.filters.update(jinja_env.filters)
+    app.jinja_env.filters.update(jinja_env.filters)
 
-  jwt = JWTManager(app)
+    jwt = JWTManager(app)
 
-  for blueprint in blueprints:
-    app.register_blueprint(blueprint)
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
 
-  for name, command in commands.items():
-    app.cli.command(name)(command)
+    for name, command in commands.items():
+        app.cli.command(name)(command)
 
-  return app
+    @app.errorhandler(NoAuthorizationError)
+    def handle_no_authorization_error(e):
+        return redirect(url_for("landing.login_view"))
+
+    return app
