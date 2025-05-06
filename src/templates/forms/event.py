39dd10cd13ad3138cb43e878e8fec9b3b2ee3d71
Alejandro Atria <alejandro.atria@enviame.io>
2025-05-06 17:38:35 -0500

    event management template and form updated

--- src/templates/forms/event.py
+++ src/templates/forms/event.py
@@ -4,17 +4,17 @@ from wtforms.validators import DataRequired, optional, NumberRange
 
 
 class EventForm(FlaskForm):
-  name = StringField('Nombre: ', validators=[DataRequired()])
-  min_price = IntegerField('Precio Mínimo: ', validators=[optional(), NumberRange(min=0)])
-  max_price = IntegerField('Precio Máximo: ', validators=[optional(), NumberRange(min=0)])
-  update = SubmitField("Actualizar Evento")
-  create = SubmitField("Crear Evento")
+  name = StringField('Name: ', validators=[DataRequired()], render_kw={"class": "form-control mb-2"})
+  min_price = IntegerField('Minimum Price: ', validators=[optional(), NumberRange(min=0)], render_kw={"class": "form-control mb-2"})
+  max_price = IntegerField('Maximum Price: ', validators=[optional(), NumberRange(min=0)], render_kw={"class": "form-control mb-2"})
+  update = SubmitField("Update Event")
+  create = SubmitField("Create Event")
   
   def validate(self):
     if not FlaskForm.validate(self):
       return False
     if self.min_price.data and self.max_price.data:
       if self.min_price.data > self.max_price.data:
-        self.min_price.errors.append("El precio mínimo no puede ser mayor que el precio máximo!")
+        self.min_price.errors.append("Minimum Price Cannot Be Higher Than Maximum Price!")
         return False
     return True
