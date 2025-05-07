1a223d1c5c0480a680342ccdc31201d02d262f3a
Alejandro Atria <alejandro.atria@enviame.io>
2025-05-07 13:25:31 -0500

    format and add delete route

--- src/controllers/wishlist_controller.py
+++ src/controllers/wishlist_controller.py
@@ -6,46 +6,75 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 from src.usecases import ManageWishlistUsecase, ManageEventsUsecase
 from src.templates.forms import WishlistForm
 
+#TODO: crear la acción para actualizar regalos individuales
+def create_wishlist_controller(
+    wishlists_usecase: ManageWishlistUsecase, 
+    events_usecase: ManageEventsUsecase
+):
+    blueprint = Blueprint("wishlist", __name__)
 
-def create_wishlist_controller(wishlists_usecase: ManageWishlistUsecase, events_usecase: ManageEventsUsecase):
-  blueprint = Blueprint("wishlist", __name__)
-  
-  @blueprint.route("/wishlist", methods=["GET", "POST"])
-  @jwt_required()
-  def wishlist_view():
-    user_id = int(get_jwt_identity())
-    events = events_usecase.get_events_by_user_id(user_id)
-    event_selected = int(request.args.get("event_id")) if request.args.get("event_id") else None
-    wishlist_form = WishlistForm()
-    if request.method == "POST":
-      form_data = request.form
-      wishes_dict = {}
-      regex = r'items-(\d+)-(\w+)'
-      for key, value in form_data.items():
-        if match := re.search(regex, key):
-          index = int(match.group(1))
-          field = match.group(2)
-          
-          if index not in wishes_dict:
-            wishes_dict[index] = {}
-          if field == "price":
-            try:
-              value = int(value)
-            except ValueError:
-              value = None
-          wishes_dict[index][field] = value
+    @blueprint.route("/wishlist", methods=["GET", "POST"])
+    @jwt_required()
+    def wishlist_view():
+        user_id = int(get_jwt_identity())
+        events = events_usecase.get_events_by_user_id(user_id)
+        event_selected = (
+            int(request.args.get("event_id")) if request.args.get("event_id") else None
+        )
+        wishlist_form = WishlistForm()
+        if request.method == "POST":
+            form_data = request.form
+            wishes_dict = {}
+            regex = r"items-(\d+)-(\w+)"
+            for key, value in form_data.items():
+                if match := re.search(regex, key):
+                    index = int(match.group(1))
+                    field = match.group(2)
 
-      wishes = [value for _, value in wishes_dict.items() if value]
+                    if index not in wishes_dict:
+                        wishes_dict[index] = {}
+                    if field == "price":
+                        try:
+                            value = int(value)
+                        except ValueError:
+                            value = None
+                    wishes_dict[index][field] = value
 
-      wishlist, error = wishlists_usecase.create_or_update_wishes(user_id, wishes)
-      if error:
-        flash(f"An Error Occurred: {error}", "error")
-        return redirect(url_for("home.home_view"))
-      else:
-        flash(f"List Updated", "success")
-      return redirect(url_for("wishlist.wishlist_view", event_id=event_selected))
-    wishlist = wishlists_usecase.get_wishlist_by_user(user_id)
-          
-    return render_template("wishlist.html", events=events, wishlist=wishlist, wishlist_form=wishlist_form)
-  
-  return blueprint
+            wishes = [value for _, value in wishes_dict.items() if value]
+
+            wishlist, error = wishlists_usecase.create(user_id, wishes)
+            if error:
+                flash(f"An Error Occurred: {error}", "error")
+                return redirect(url_for("home.home_view"))
+            else:
+                flash(f"List Updated", "success")
+            return redirect(url_for("wishlist.wishlist_view", event_id=event_selected))
+        wishlist = wishlists_usecase.get_wishlist_by_user(user_id)
+
+        return render_template(
+            "wishlist.html",
+            events=events,
+            wishlist=wishlist,
+            wishlist_form=wishlist_form,
+        )
+
+    @blueprint.route("/delete-item", methods=["POST"])
+    @jwt_required()
+    def delete_item():
+        user_id = int(get_jwt_identity())
+        item_id = request.args.get("item_id")
+        if not item_id:
+            flash("Item ID is required", "error")
+            return redirect(url_for("home.home_view"))
+        wish = wishlists_usecase.get_wish_by_user_and_id(user_id, item_id)
+        if not wish:
+            flash("Item not found", "error")
+            return redirect(url_for("wishlist.wishlist_view"))
+        _, error = wishlists_usecase.delete(wish.id)
+        if error:
+            flash(f"An Error Occurred: {error}", "error")
+        else:
+            flash(f"Item Deleted", "success")
+        return redirect(url_for("wishlist.wishlist_view"))
+
+    return blueprint
