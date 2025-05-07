7d8084157c4e0e44f15e60386ae872335b78f4c6
Alejandro Atria <alejandro.atria@enviame.io>
2025-05-07 11:52:50 -0500

    fix price range filters as special_filters

--- src/repositories/sqlalchemy_wishlist_repository.py
+++ src/repositories/sqlalchemy_wishlist_repository.py
@@ -9,10 +9,11 @@ class SQLAlchemyWishlistRepository(SQLAlchemyBaseRepository):
         
     def get_wishlist_by_range(self, user_id: int, min_price: int|None, max_price: int|None) -> list[Wish]:
         filter = {"user_id": user_id, "deleted_at": None}
+        special_filters = []
         if min_price and max_price:
-            filter["price"] = {"$gte": min_price, "$lte": max_price}
+            special_filters.append(WishModel.price.between(min_price, max_price))
         elif min_price:
-            filter["price"] = {"$gte": min_price}
+            special_filters.append(WishModel.price >= min_price)
         elif max_price:
-            filter["price"] = {"$lte": max_price}
-        return self.get(filters=filter)
+            special_filters.append(WishModel.price <= max_price)
+        return self.get(filters=filter, special_filters=special_filters)
